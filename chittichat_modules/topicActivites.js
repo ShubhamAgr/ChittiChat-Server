4c547230b5685d9e6e1e735010b6c1f3be4b86fe
ShubhamAgr <agrawal.shubham291@gmail.com>
2016-12-06 17:11:23 +0530

    attempt to add socket

--- chittichat_modules/topicActivites.js
+++ chittichat_modules/topicActivites.js
@@ -64,14 +64,15 @@ exports.newArticle = function(userId,topicId,marticle_content,socketMap,io,callb
         // console.log(socketMap.get("shubham"));
         console.log(model.toObject().group_id);
         // socketMap.get("shubham").emit('newarticle',{"articleId":id});
-          console.log(io);
-          io.to(model.toObject().group_id).emit('newarticle',{"articleId":id});
+          // console.log(io);
+          // io.to(model.toObject().group_id).emit('newarticle',{"articleId":id});
         // socketMap.get("shubham").broadcast.in(model.toObject().group_id).emit('newarticle',{"articleId":id});
         //  io.in(model.room_id).emit("newarticle",{"articleId":id});
         userModel.findByIdAndUpdate(userId,{$addToSet:{"myarticles":id},safe:true,upsert:true},function(err){
           if(err){
             callback({"message":"unsuccessful"});
           }else{
+              socketMap.get("shubham").emit('newarticle',{"articleId":id});
               callback({"message":"successful"});
           }
         });
