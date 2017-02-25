d27af47af577216236a88e3d793d815e08a8f33e
ShubhamAgr <agrawal.shubham291@gmail.com>
2017-02-25 16:33:48 +0530

    bug fix

--- chittichat_modules/groupActivites.js
+++ chittichat_modules/groupActivites.js
@@ -225,7 +225,7 @@ exports.accept_request = function(groupId,requestedBy,callback){
   userModel.find({'_id':requestedBy},function(err,user){
     var userObject = user[0].toObject();
     for(var i=0;i<userObject.groups.length;i++){
-      if(userObject.groups[i]._id==groupId && userObject.groups[i].role == "follower"){
+      if(userObject.groups[i]._id==groupId){
         console.log(userObject.groups[i]._id);
         userModel.findByIdAndUpdate(userId,{$pull:{"groups":{_id:mongoose.Types.ObjectId(groupId)}}},{safe:true,upsert:true},function(err){
           if(err){
