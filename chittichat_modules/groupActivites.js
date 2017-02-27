e5d59d47f972fa719a4cd5888aa7d61d197ca907
ShubhamAgr <agrawal.shubham291@gmail.com>
2017-02-27 11:24:31 +0530

    update

--- chittichat_modules/groupActivites.js
+++ chittichat_modules/groupActivites.js
@@ -264,7 +264,7 @@ exports.accept_request = function(groupId,requestedBy,callback){
     var userObject = user[0].toObject();
     if(userObject.groups.length != 0){
     for(var i=0;i<userObject.groups.length;i++){
-      if(userObject.groups[i]._id==groupId){
+      if(userObject.groups[i]._id==mongoose.Types.ObjectId(groupId)){
         console.log(userObject.groups[i]._id);
         userModel.findByIdAndUpdate(userId,{$pull:{"groups":{_id:mongoose.Types.ObjectId(groupId)}}},{safe:true,upsert:true},function(err){
           if(err){
